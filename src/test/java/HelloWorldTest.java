a66e2d47e01e990da49c37c94edc4f3c28f7692d
kewong <kenz.yihhang@gmail.com>
2021-06-03 21:13:39 +0800

    Added test case

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,11 @@
+public class HelloWorldTest {
+
+    public void testHello(){
+        System.out.println("TEST - testHello");
+    }
+
+    public void testWorld(){
+        System.out.println("TEST - testWorld");
+    }
+
+}
